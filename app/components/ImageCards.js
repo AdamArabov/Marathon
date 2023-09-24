8e5e78a86e9e68153ce8fbfe413fe61ca9e169ca
AdamArabov <arabovadam@yahoo.com>
2023-09-24 11:45:11 -0400

    Changed Up Some stuff

--- app/components/ImageCards.js
+++ app/components/ImageCards.js
@@ -4,33 +4,36 @@ import { motion } from "framer-motion";
 import { useState } from "react";
 
 export default function ImageCard () {
-    const [expandedIndex, setExpandedIndex] = useState(null)
+    const [expandedIndex, setExpandedIndex] = useState(0)
     const handleCardClick = (index) => {
         setExpandedIndex(index === expandedIndex ?-1 : index)
     }
 
     const cardVariants = {
         expanded: {
-            width: "600px"
+            width: "3000px"
         },
         collasped: {
-            width: "300px"
+            width: "500px"
         }
     }
 
-    const cardImages = ["/marathon.jpg", "/marathon2.jpg"]
+    const cardImages = ["/marathon.jpg", "/marathon2.jpg","/marathon3.jpg", "/marathon4.jpg"]
     const cardDescriptions = [
         'YOU ARE',
-        'NOT SAFE'
+        'NOT SAFE',
+        'We ARE',
+        'WATCHING',
+
     ]
     return(
-        <section className="py-16 pb-[300px]">
+        <section className=" pb-[300px]">
           
             <div className="mt-12 flex flex-col md:flex-row justify-center items-center gap-5">
-  {[0, 1].map((index) => (
+  {[0, 1,2,3].map((index) => (
     <motion.div
       key={index} 
-      className={`card cursor-pointer h-[500px] bg-cover bg-center rounded-[20px]${index === expandedIndex ? ' expanded' : ''}`}
+      className={`card cursor-pointer h-[500px] bg-cover bg-center ${index === expandedIndex ? ' expanded' : ''}`}
       variants={cardVariants}
       initial="collapsed" 
       animate={index === expandedIndex ? 'expanded' : 'collapsed'}
@@ -41,8 +44,8 @@ export default function ImageCard () {
       }}
     >
         <div className=" card-content h-full flex flex-col justify-end">
-            <div className="card-footer rounded-b-[20px] bg-gray-800 bg-opacity-75 min-h-[100px] flex flex-col items-center justify-center">
-              <h2 className="text-xl font-semibold text-white text-center">Card {index + 1}</h2>  
+            <div className="card-footer rounded-b-[20px] min-h-[100px] flex flex-col items-center justify-center">
+              <h2 className="text-xl font-semibold text-white text-center">{index * 777}</h2>  
               {index === expandedIndex && (
                 <p className="mt-2 text-gray-300 text-center">{cardDescriptions[index]} </p>
               )
