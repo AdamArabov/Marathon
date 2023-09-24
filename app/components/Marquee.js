c2040843aeb9f0712f9382357ba7e67fd7867c4a
AdamArabov <arabovadam@yahoo.com>
2023-09-24 13:18:22 -0400

    Hidden Messages added

--- app/components/Marquee.js
+++ app/components/Marquee.js
@@ -2,21 +2,21 @@ export default function Marquee() {
     return (
       <div className="bg-marathon2  overflow-hidden">
         <div className=" animate-marquee whitespace-nowrap flex ">
-          <span className="text-black text-4xl mx-4">Marquee Item 1</span>
-          <span className="text-black text-4xl mx-4">Marquee Item 2</span>
-          <span className="text-black text-4xl mx-4">Marquee Item 3</span>
-          <span className="text-black text-4xl mx-4">Marquee Item 4</span>
-          <span className="text-black text-4xl mx-4">Marquee Item 5</span>
-          <span className="text-black text-4xl mx-4">Marquee Item 1</span>
-        <span className="text-black text-4xl mx-4">Marquee Item 2</span>
-        <span className="text-black text-4xl mx-4">Marquee Item 3</span>
-        <span className="text-black text-4xl mx-4">Marquee Item 4</span>
-        <span className="text-black text-4xl mx-4">Marquee Item 5</span>
-        <span className="text-black text-4xl mx-4">Marquee Item 1</span>
-        <span className="text-black text-4xl mx-4">Marquee Item 2</span>
-        <span className="text-black text-4xl mx-4">Marquee Item 3</span>
-        <span className="text-black text-4xl mx-4">Marquee Item 4</span>
-        <span className="text-black text-4xl mx-4">Marquee Item 5</span>
+          <span className="text-black text-4xl mx-4">WELCOME RUNNERS!</span>
+          <span className="text-black text-4xl mx-4">GREATNESS AWAITS YOU!</span>
+          <span className="text-black text-4xl mx-4">DO YOU HAVE WHAT IT TAKES?</span>
+          <span className="text-black text-4xl mx-4">WILL YOU SURVIVE?</span>
+          <span className="text-black text-4xl mx-4">OR PERISH LIKE THE REST?</span>
+          <span className="text-black text-4xl mx-4">SEVMUCE=</span>
+        <span className="text-black text-4xl mx-4">VEhFWSBBUkUgTFlJTkcgVE8gWU9V</span>
+        <span className="text-black text-4xl mx-4">QUxMIFRIQVQgQVdBSVRTIFlPVQ==</span>
+        <span className="text-black text-4xl mx-4">WU9VIFdJTEwgQkUgVEVTVEVE</span>
+        <span className="text-black text-4xl mx-4">WU9VIFdJTEwgRkFJTA==</span>
+        <span className="text-black text-4xl mx-4">QUxMIA==</span>
+        <span className="text-black text-4xl mx-4">V0lMTCBCRQ==</span>
+        <span className="text-black text-4xl mx-4">QU5TV0VSRUQg</span>
+        <span className="text-black text-4xl mx-4">U09PTg==</span>
+        <span className="text-black text-4xl mx-4">UlVOTkVSLCBHT09EIExVQ0s=</span>
         </div>
       </div>
     );
