2b39fb59b5604eda99b3fa1232f009bf96822a73
AdamArabov <arabovadam@yahoo.com>
2023-09-25 22:54:30 -0400

    Added to the News Section

--- app/components/News.js
+++ app/components/News.js
@@ -8,7 +8,7 @@ export default function News () {
         <div className="h-full  px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
          
           <h1 className="title-font sm:text-2xl text-xl font-medium text-marathon mb-3 font-marathon">Lore</h1>
-          <p className="text-marathon2 leading-relaxed mb-3">Explore the Lore of the Marathon Universe.</p>
+          <p className="text-marathon2 leading-relaxed mb-3">Explore the Lore of the Marathon Universe. Maintained by the legend Hamish Sinclair.</p>
           <a href="https://marathon.bungie.org/story/newjune23.html" className="text-marathon inline-flex items-center" target="_blank">Explore
             <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
               <path d="M5 12h14"></path>
@@ -21,7 +21,7 @@ export default function News () {
         <div className="h-full  px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
          
           <h1 className="title-font sm:text-2xl text-xl font-medium text-marathon mb-3 font-marathon">News</h1>
-          <p className="text-marathon2 leading-relaxed mb-3">Follow the Latest News Updates</p>
+          <p className="text-marathon2 leading-relaxed mb-3">Follow the Latest News Updates from Bungie about Marathon</p>
           <a href="https://www.marathonthegame.com/y3vmGPNRxH3RNTqLkq5PFXZy" className="text-marathon inline-flex items-center" target="_blank">Learn More
             <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
               <path d="M5 12h14"></path>
@@ -35,7 +35,7 @@ export default function News () {
         <div className="h-full px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
          
           <h1 className="title-font sm:text-2xl text-xl font-medium text-marathon mb-3 font-marathon">Social</h1>
-          <p className="text-marathon2 leading-relaxed mb-3">Connect with the Community</p>
+          <p className="text-marathon2 leading-relaxed mb-3">Connect with the Awesome Community and Discuss and Share.</p>
           <a  href="https://www.reddit.com/r/Marathon/"className="text-marathon inline-flex items-center" target="_blank">Join
             <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
               <path d="M5 12h14"></path>
